677efab2bb6124eb3a7470cc291c7c5fdb8fbd00
romaingrude <romaingrude@yahoo.fr>
2025-11-28 03:03:50 +0000

    Restored default TARGET_IP_ADDRESS

--- utils/config.py
+++ utils/config.py
@@ -9,8 +9,8 @@ def _require_env(var_name: str) -> str:
 
 AUTH_KEY = _require_env('AUTHKEY_SERVER_WEBSITE')  # Secure key for authentication
 TARGET_MAC_ADDRESS = _require_env('TARGET_MAC_ADDRESS_SERVER')  # Your PC's MAC address for Wake On Lan
-TARGET_IP_ADDRESS = "127.0.0.1"
-# TARGET_IP_ADDRESS = _require_env('TARGET_IP_ADDRESS_SERVER')  # Your PC's IP address
+# TARGET_IP_ADDRESS = "127.0.0.1" # For local testing
+TARGET_IP_ADDRESS = _require_env('TARGET_IP_ADDRESS_SERVER')  # Your PC's IP address
 TARGET_FLASK_SERVER_PORT = _require_env('TARGET_FLASK_SERVER_PORT')
 LOGIN_USERNAME = _require_env('LOGIN_USERNAME')
 LOGIN_PASSWORD = _require_env('LOGIN_PASSWORD')
